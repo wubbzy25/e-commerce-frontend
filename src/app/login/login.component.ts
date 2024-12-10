2164834f150f1420ca52f831087b91949da06a25
Wuubzi <carlosasalas321@gmail.com>
2024-12-09 22:44:12 -0500

    testing login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,11 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
