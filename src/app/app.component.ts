d4207a235875fe17070c8b3d2df130d0928653d9
Wuubzi <carlosasalas321@gmail.com>
2024-12-09 19:20:13 -0500

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,10 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Front-End';
+  saludar() {
+    return alert('Hola 🙌');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: AppComponent,
+  },
+  {
+    path: 'register',
+    title: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    title: 'login',
+    component: LoginComponent,
+  },
+];
